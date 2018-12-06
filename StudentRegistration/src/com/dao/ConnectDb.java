494bc29cb5e29bf4de9dfdee124e4fd2690ff990
manikanta <manikanta@imnotout.com>
2018-12-06 21:07:13 +0530

    second commit

--- StudentRegistration/src/com/dao/ConnectDb.java
+++ StudentRegistration/src/com/dao/ConnectDb.java
@@ -1,8 +1,12 @@
 package com.dao;
-
+// this class is created for doing all the CURD operations into the files 
 public class ConnectDb {
    
 	
 	
+//this method is used for creating the   the connection to the file  
 	
 }
+
+
+
